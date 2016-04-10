9f916806b2db33f903d9accc353144622a60d908
mmellott <matthew.mellott@yahoo.com>
2016-04-10 11:09:51 -0700

    first commit!

---
+++ self.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+
+@tape = 'puts "#!/usr/bin/ruby"
+puts
+puts "@tape = " + 39.chr + @tape + 39.chr
+puts
+puts @tape
+
+'
+
+puts "#!/usr/bin/ruby"
+puts
+puts "@tape = " + 39.chr + @tape + 39.chr
+puts
+puts @tape
+
