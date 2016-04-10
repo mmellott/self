5b63fa4b1bdda295e3fa183efb79c5887f885305
mmellott <matthew.mellott@yahoo.com>
2016-04-10 12:54:00 -0700

    add self for python

---
+++ self.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+tape = """print "#!/usr/bin/python"
+print
+print "tape = " + chr(34)*3 + tape + chr(34)*3
+print
+print tape
+"""
+
+print "#!/usr/bin/python"
+print
+print "tape = " + chr(34)*3 + tape + chr(34)*3
+print
+print tape
+
